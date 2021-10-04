4c84af2c5ae90f3aacc1f30c476596e6ff28916e
Ayush <ayushk2023@gmail.com>
2021-10-04 19:44:51 -0400

    change the motor speed and turning sensitivity.

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -43,7 +43,7 @@ public class Drivetrain extends SubsystemBase {
   }
 
   public void arcadeDrive(double xaxisSpeed, double zaxisRotate) {
-    m_diffDrive.arcadeDrive(xaxisSpeed, zaxisRotate);
+    m_diffDrive.arcadeDrive(xaxisSpeed * -16, zaxisRotate * -16);
   }
 
   public void resetEncoders() {
