e4e53ef7d622e9d16c0b4477d5a24cc1fcc09c75
ADVERD5 <advaithdasu5@gmail.com>
2021-11-11 20:54:03 -0500

    Create DrivetrainIO and DrivetrainIORomi

--- src/main/java/frc/robot/subsystems/DrivetrainIORomi.java
+++ src/main/java/frc/robot/subsystems/DrivetrainIORomi.java
@@ -4,7 +4,36 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.BuiltInAccelerometer;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.Spark;
+import frc.robot.sensors.RomiGyro;
+
 /** Add your docs here. */
-public class DrivetrainIORomi implements DrivetrainIO {
-    
+public class DrivetrainIORomi implements DrivetrainIO {  
+    private final Spark m_leftMotor = new Spark(0);
+    private final Spark m_rightMotor = new Spark(1);
+    private final RomiGyro m_gyro = new RomiGyro();
+    private final Encoder m_leftEncoder = new Encoder(4, 5);
+    private final Encoder m_rightEncoder = new Encoder(6, 7);
+
+    public void updateInputs(DrivetrainIOInputs inputs) {
+        inputs.leftPositionRadians = m_leftEncoder.get();
+        inputs.rightPositionRadians = m_rightEncoder.get();
+        inputs.gyroPositionRadians = m_gyro.getAngleZ();
+    }
+
+    public void setOutputs(double leftPercent, double rightPercent) {
+        m_leftMotor.set(leftPercent);
+        m_rightMotor.set(rightPercent);
+    }
+  
+    public void resetEncoders() {
+        m_leftEncoder.reset();
+        m_rightEncoder.reset();
+    }
+  
+    public void resetGyro() {
+        m_gyro.reset();
+    }
 }
