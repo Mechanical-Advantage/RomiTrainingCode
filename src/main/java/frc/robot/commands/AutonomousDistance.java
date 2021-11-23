c9d2bd489bea3ab318bc99a144ccf9f9772aa2d0
Aum Patel <aumpatel130806@gmail.com>
2021-11-22 20:34:14 -0500

    Co-authored-by: V-RA <V-RA@users.noreply.github.com>

--- src/main/java/frc/robot/commands/AutonomousDistance.java
+++ src/main/java/frc/robot/commands/AutonomousDistance.java
@@ -9,16 +9,16 @@ import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 
 public class AutonomousDistance extends SequentialCommandGroup {
   /**
-   * Creates a new Autonomous Drive based on distance. This will drive out for a specified distance,
-   * turn around and drive back.
+   * Creates a new Autonomous Drive based on distance. This will drive out for a
+   * specified distance, turn around and drive back.
    *
    * @param drivetrain The drivetrain subsystem on which this command will run
    */
   public AutonomousDistance(Drivetrain drivetrain) {
-    addCommands(
-        new DrivePID(10, drivetrain),
-        new TurnDegrees(-0.5, 180, drivetrain),
-        new DrivePID(10, drivetrain),
-        new TurnDegrees(0.5, 180, drivetrain));
+    addCommands( 
+    new DrivePID(10, drivetrain), 
+    new TurnPID(180, drivetrain), 
+    new DrivePID(10, drivetrain),
+    new TurnPID(180, drivetrain));
   }
 }
--- src/main/java/frc/robot/commands/TurnPID.java
+++ src/main/java/frc/robot/commands/TurnPID.java
@@ -4,29 +4,51 @@
 
 package frc.robot.commands;
 
+import frc.robot.subsystems.Drivetrain;
+import edu.wpi.first.wpilibj.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
 public class TurnPID extends CommandBase {
+  private static final double KP = 0.01;
+  private static final double KD = 0;
+  private static final double TOLERANCE = 2;
+  private final Drivetrain m_drive;
+  private final double m_degrees;
+  private final PIDController pid = new PIDController(KP, 0, KD);
+
   /** Creates a new TurnPID. */
-  public TurnPID() {
+  public TurnPID(double degrees, Drivetrain drive) {
+    m_degrees = degrees;
+    m_drive = drive;
+    addRequirements(drive);
     // Use addRequirements() here to declare subsystem dependencies.
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+    m_drive.arcadeDrive(0, 0);
+    m_drive.resetGyro();
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+    double output = pid.calculate(m_drive.getGyroAngleZ(), m_degrees);
+    m_drive.arcadeDrive(0, output);
+    System.out.println(output);
+  }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    m_drive.arcadeDrive(0, 0);
+  }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return false;
+    double gyroAngle = m_drive.getGyroAngleZ();
+    return Math.abs(gyroAngle) <= m_degrees + TOLERANCE && Math.abs(gyroAngle) >= m_degrees - TOLERANCE;
   }
 }
