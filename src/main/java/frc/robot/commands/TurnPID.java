5d7ec163de0fb91c6418fa2a3b8193664cd198a8
Rohan Sundaresan <rohansundaresan35@gmail.com>
2021-12-06 20:35:07 -0500

    add setpoint and finish TurnPID command

--- src/main/java/frc/robot/commands/TurnPID.java
+++ src/main/java/frc/robot/commands/TurnPID.java
@@ -4,29 +4,51 @@
 
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Drivetrain;
 
 public class TurnPID extends CommandBase {
+  private final double targetAngleDegrees;
+  private final double pidTolerance = 1;
+  private static final double kP = 0.1;
+  private static final double kI = 0;
+  private static final double kD = 0;
+  private final Drivetrain drive;
+  private final PIDController pid = new PIDController(kP, kI, kD);
+
   /** Creates a new TurnPID. */
-  public TurnPID() {
+  public TurnPID(Drivetrain drive, double targetAngleDegrees) {
     // Use addRequirements() here to declare subsystem dependencies.
+    this.targetAngleDegrees = targetAngleDegrees;
+    this.drive = drive;
+    addRequirements(drive);
+    pid.setSetpoint(targetAngleDegrees);
+    pid.setTolerance(pidTolerance);
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+    drive.resetGyro();
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+    double percentOut = pid.calculate(drive.getGyroAngleZ());
+    drive.arcadeDrive(0,percentOut);
+  }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    drive.arcadeDrive(0,0);
+  }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return false;
+    return pid.atSetpoint();
   }
 }
