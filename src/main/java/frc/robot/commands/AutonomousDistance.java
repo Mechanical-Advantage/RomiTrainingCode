9eb3a93d70c6a9de75c608f7a46330569836abe6
purpleBear-mia <miayumengli@gmail.com>
2023-09-14 20:56:10 -0400

    Lesson 09-10

--- src/main/java/frc/robot/commands/AutonomousDistance.java
+++ src/main/java/frc/robot/commands/AutonomousDistance.java
@@ -9,7 +9,8 @@ import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 
 public class AutonomousDistance extends SequentialCommandGroup {
   /**
-   * Creates a new Autonomous Drive based on distance. This will drive out for a specified distance,
+   * Creates a new Autonomous Drive based on distance. This will drive out for a
+   * specified distance,
    * turn around and drive back.
    *
    * @param drivetrain The drivetrain subsystem on which this command will run
@@ -17,8 +18,12 @@ public class AutonomousDistance extends SequentialCommandGroup {
   public AutonomousDistance(Drivetrain drivetrain) {
     addCommands(
         new DriveDistance(-0.5, 10, drivetrain),
-        new TurnDegrees(-0.5, 180, drivetrain),
+        new TurnDegrees(-0.5, 90, drivetrain),
         new DriveDistance(-0.5, 10, drivetrain),
-        new TurnDegrees(0.5, 180, drivetrain));
+        new TurnDegrees(-0.5, 90, drivetrain),
+        new DriveDistance(-0.5, 10, drivetrain),
+        new TurnDegrees(-0.5, 90, drivetrain),
+        new DriveDistance(-0.5, 10, drivetrain),
+        new TurnDegrees(-0.5, 90, drivetrain));
   }
 }
--- src/main/java/frc/robot/subsystems/Apps2.java
+++
@@ -1,7 +0,0 @@
-package frc.robot.subsystems;
-
-public class Apps2 {
-    for (int a=0; a<9; a++){
-        System.out.println(a);
-    }
-}
